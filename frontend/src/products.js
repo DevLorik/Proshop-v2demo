214bff79126a88e3215f70f69bb8817b8db749c7
DevLorik <devlorikhalili@gmail.com>
2023-10-18 21:24:06 +0200

    pushed code

---
+++ frontend/src/products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    _id: "1",
+    name: "Bose Headphones",
+    image: "/images/Headphones.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly.High-quality AAC audio offers immersive listening experience",
+    brand: "Bose",
+    category: "Electronics",
+    price: 199.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Apple Headphones",
+    image: "/images/AppleHeadphones.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly.High-quality AAC audio offers immersive listening experience",
+    brand: "Apple",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 0,
+    rating: 5.0,
+    numReviews: 10,
+  },
+];
+export default products;
